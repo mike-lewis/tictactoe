b23c597a91783d17774c3064fe85fa45821633b0
mike-lewis <mike-lewis@users.noreply.github.com>
2016-01-23 11:54:59 -0800

    Update index.php

--- index.php
+++ index.php
@@ -2,35 +2,12 @@
 
 	require_once('Game.php');
 
-	
 	$name = 'Mike';
 	$what = 'geek';
 	$level = 10;
 	echo 'Hi, my name is ' . $name, ' , and I am a level ' . $level . ' ' . $what;
-	
-	// --- This is code is from the beginning of the tutorial, left commented for reference ---
-	
-	//$hoursworked = $_GET['hours'];
-	//$rate = 12;
-	//$total = $hoursworked * $rate;
-	//$position = $_GET['board'];
-	//$squares = str_split($position);
-	
-	//if ($hoursworked > 40) {
-	//	$total = $hoursworked * $rate * 1.5;
-	//} else {
-	//	$total = $hoursworked * $rate;
-	//}
-	//echo '<br/>';
-	//echo ($total > 0) ? 'You owe me ' . $total : "You're welcome";
 
 	echo '<br/>';
-	
-	// --- Old game logic ---
-	
-	//if (winner('x', $squares)) echo 'You win.';
-	//else if (winner('o', $squares)) echo 'I win.';
-	//else echo 'No winner yet.';
 	
 	// --- New game logic ---
 	
@@ -99,42 +76,5 @@
 				   ($position[8] == $token)) {
 						$won = true;
 		}
-
-		
-		/*
-		$result = true;
-		for ($row = 0; $row < 3; $row++) {
-			
-			for ($col = 0; $col < 3; $col++) {
-				if ($position[3 * $row + $col] != $token) {
-					$result = false;	
-					break;
-				}
-			}
-		}
-		
-		if($result) {
-			return $result;
-		}
-		for ($col = 0; $col < 3; $col++) {
-			
-			for ($row = 0; $row < 3; $row++) {
-				if ($position[3 * $row + $col] != $token) {
-					$result = false;	
-					break;
-				}
-			}
-		}
-		
-		if($result) {
-			return $result;
-		}
-		
-		return $result;
-		*/
-		
-		//return $won;
-	}
-	
 	
 ?>
